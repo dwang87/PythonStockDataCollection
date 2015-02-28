dd23e46b63369cda823194685568738a81414ed9
dwang87 <wdno01@gmail.com>
2015-02-28 23:00:13 +0800

    Get Prices From Multiple Quotes
    define a stock quote array and foreach quote to get each price

---
+++ GetPricesFromMultiQuotes.py
@@ -0,0 +1,24 @@
+﻿# -*- coding: utf-8-*-
+# @Date    : 2015-05-20
+# @Author  : Chris
+# @Website : 
+# @Description:  Get Apple price from Yahoo Finance
+# @Tools-Required: re, urllib
+import urllib
+import re
+
+symbolslist = ["aapl","spy","goog","nflx","qihu","yy"]
+
+i=0
+while i<len(symbolslist):
+    htmlfile = urllib.urlopen("http://finance.yahoo.com/q?s="+symbolslist[i] +"&ql=1")
+    htmltext = htmlfile.read()
+
+    regex = '<span id="yfs_l84_'+symbolslist[i]+'">(.+?)</span>'
+
+    pattern = re.compile(regex)
+
+    price = re.findall(pattern, htmltext)
+
+    print price
+    i+=1
