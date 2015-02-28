6bbe2d4bb3af257cbd2dc8b78ae5991f567262b1
dwang87 <wdno01@gmail.com>
2015-02-28 22:42:04 +0800

    Basic example to get price from a quote
    A very basic example to get price from a quote from yahoo

---
+++ GetPriceFromSingleQuote.py
@@ -0,0 +1,21 @@
+﻿# -*- coding: utf-8-*-
+# @Date    : 2015-05-20
+# @Author  : Chris
+# @Website : 
+# @Description:  Get Apple price from Yahoo Finance
+# @Tools-Required: re, urllib
+import urllib
+import re
+
+
+
+htmlfile = urllib.urlopen("http://finance.yahoo.com/q?s=AAPL&ql=1")
+htmltext = htmlfile.read()
+
+regex = '<span id="yfs_l84_aapl">(.+?)</span>'
+
+pattern = re.compile(regex)
+
+price = re.findall(pattern, htmltext)
+
+print price
