68f885a11f5ba0a078d4b809b1d5e7781d3659ae
dwang87 <wdno01@gmail.com>
2015-03-02 00:16:37 +0800

    Get prices from Bloomberg
    Stock data is retrieved in JSON format.

---
+++ GetSingleQuotePriceFromBloomberg.py
@@ -0,0 +1,15 @@
+﻿# -*- coding: utf-8-*-
+# @Date    : 2015-02-20
+# @Author  : Chris
+# @Website : 
+# @Description:  Get prices from Bloomberg
+# @Tools-Required: re, urllib
+import urllib
+import re
+import json
+
+htmltext = urllib.urlopen("http://www.bloomberg.com/markets/watchlist/recent-ticker/AAPL:US")
+
+data = json.load(htmltext)
+
+print data['last_price']
