ba8a69153a76c40260b902274ce0c6ba5be9e487
dwang87 <wdno01@gmail.com>
2015-03-01 23:52:09 +0800

    Get Stock Price from Google Finance

---
+++ GetSingleQuotePriceFromGoogleFinance.py
@@ -0,0 +1,14 @@
+﻿# -*- coding: utf-8-*-
+# @Date    : 2015-02-20
+# @Author  : Chris
+# @Website : 
+# @Description:  Get prices from Google Finance
+# @Tools-Required: re, urllib
+import urllib
+import re
+
+
+htmlfile = urllib.urlopen("http://www.google.com/finance/getprices?q=GOOG&x=NASD&i=10&p=25m&f=c")
+htmltext = htmlfile.read()
+
+print htmltext.split()[len(htmltext.split())-1]
