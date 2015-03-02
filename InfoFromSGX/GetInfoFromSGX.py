4c1ae2491a27e9028f18d0a0aa319eabc6934a42
dwang87 <wdno01@gmail.com>
2015-03-03 00:50:23 +0800

    All Listed Companies in SGX added

--- InfoFromSGX/GetInfoFromSGX.py
+++ InfoFromSGX/GetInfoFromSGX.py
@@ -8,11 +8,20 @@ import urllib
 import re
 import json
 
-htmlfile= urllib.urlopen("http://www.sgx.com/proxy/SgxDominoHttpProxy?timeout=100&dominoHost=http%3A%2F%2Finfofeed.sgx.com%2FApps%3FA%3DCow_CorporateInformation_Content%26B%3DCorpDistributionByCompanyNameCategoryAndExYear%26R_C%3DSATS%20LTD.%26C_T%3D20")
+htmlfile= urllib.urlopen("http://www.sgx.com/proxy/SgxDominoHttpProxy?timeout=100&dominoHost=http%3A%2F%2Finfofeed.sgx.com%2FApps%3FA%3DCow_CorporateInformation_Content%26B%3DCorpDistributionByCompanyNameCategoryAndExYear%26R_C%3DSTI ETF%26C_T%3D500")
 
 htmltext = htmlfile.read()
 
 newhtmltext = json.loads(htmltext.replace("{}&&",""))
 
-print newhtmltext["items"]
+print len(newhtmltext["items"])
+
+i=0
+while i < len(newhtmltext["items"]):
+
+    if newhtmltext["items"][i]!= {}:
+        print newhtmltext["items"][i]['Particulars'],"ex-div date",newhtmltext["items"][i]['Ex_Date']
+      
+    i+=1
+     
 
